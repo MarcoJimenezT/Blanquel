8b25c47cdfd574da71b0e38e99ed09c61ca48ec3
MarcoJim <marcogiovannij@hotmail.com>
2021-09-30 17:11:22 -0500

    Sitio con animaciones en index

---
+++ docs/js/main.js
@@ -0,0 +1,40 @@
+gsap.to(".charlemos",{
+    duration: 1,
+    right: "20px",
+    delay: 3.2,
+    ease: 'bounce.out',
+});
+gsap.to(".titulo-header",{
+    duration: 1,
+    opacity: "1",
+    "margin-top": "50px",
+    delay: 2.8,
+    ease: 'expo',
+});
+gsap.to(".nav_menu",{
+    duration: .5,
+    opacity: "1",
+    right: "0",
+    delay: 1,
+    ease: 'expo',
+});
+gsap.to(".nav_menu",{
+    duration: .5,
+    opacity: "1",
+    height: "80px",
+    delay: 1.5,
+    ease: 'expo',
+});
+gsap.to(".logotiposkew",{
+    duration: .5,
+    opacity: "1",
+    left: "0",
+    delay: 1,
+    ease: 'expo',
+});
+gsap.to(".nav_items",{
+    duration: 1,
+    opacity: "1",
+    delay: 2.5,
+    ease: 'expo',
+});
